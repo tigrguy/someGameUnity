8a9aa111f82881d4d6cdcb286d7999acea749564
byArdena <Lera.byr@yandex.ru>
2024-04-10 23:32:18 +0300

    ну тип передвижение
    кубик делает туда-сюда

---
+++ Assets/Script/PlayerControler.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class PlayerControler : MonoBehaviour
+{
+    public float speed;
+    
+    public float moveInput;
+
+    private Rigidbody2D rb;
+    public Joystick joystick;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    
+    private void Update()
+    {
+        moveInput = joystick.Horizontal;
+        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+    }
+    
+}
