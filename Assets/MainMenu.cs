2c07a84a5e2cdbea86bc579a34c9053a5df11586
tigrguy <57717383+tigrguy@users.noreply.github.com>
2024-05-20 20:33:25 +0300

    оно и ранд-ом-ном-ном и асинхронно ЧЕЕ
    старт из главного меню подгружает случайную сцену вне боя (у нас их ток две) и делает это асинхронно

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
 
 public class MainMenu : MonoBehaviour
 {
@@ -14,4 +16,36 @@ public class MainMenu : MonoBehaviour
     {
         Application.Quit();
     }
+
+
+
+    public void LoadRandomSceneAsync()
+    {
+        // Массив сцен, из которых будем выбирать случайную
+        string[] scenes = { "1Scene", "2Scene2" };
+
+        // Выбираем случайную сцену
+        int randomIndex = Random.Range(0, scenes.Length);
+        string randomScene = scenes[randomIndex];
+
+        // Запускаем асинхронную загрузку
+        StartCoroutine(LoadSceneAsync(randomScene));
+    }
+
+    private IEnumerator LoadSceneAsync(string sceneName)
+    {
+        // Начинаем асинхронную загрузку сцены
+        AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(sceneName);
+
+        // Пока сцена не загружена
+        while (!asyncOperation.isDone)
+        {
+            // Можно добавить код для отображения индикатора загрузки
+            // float progress = Mathf.Clamp01(asyncOperation.progress / 0.9f);
+            // Debug.Log("Loading progress: " + (progress * 100) + "%");
+
+            yield return null;
+        }
+    }
+
 }
